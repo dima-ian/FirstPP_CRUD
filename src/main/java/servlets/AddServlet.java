6ceac688da26478d4344f071d7b1c2eaac80134b
Dima Ian <ltr.tsts@mail.ru>
2020-02-14 20:58:43 +0300

    links added

--- src/main/java/servlets/AddServlet.java
+++ src/main/java/servlets/AddServlet.java
@@ -1,11 +1,25 @@
 package servlets;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-@WebServlet("/adduser")
+//@WebServlet("/FirstPP_CRUD_war_exploded/adduser")
 public class AddServlet extends HttpServlet {
 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String path = "/notfound";
+        ServletContext servletContext = getServletContext();
+        RequestDispatcher requestDispatcher = servletContext.getRequestDispatcher(path);
+        requestDispatcher.forward(request, response);
+    }
     
 
 }
--- src/main/java/servlets/ListServlet.java
+++ src/main/java/servlets/ListServlet.java
@@ -1,10 +1,25 @@
 package servlets;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-@WebServlet("/allusers")
+//@WebServlet("/FirstPP_CRUD_war_exploded/allusers")
 public class ListServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String path = "/notfound";
+        ServletContext servletContext = getServletContext();
+        RequestDispatcher requestDispatcher = servletContext.getRequestDispatcher(path);
+        requestDispatcher.forward(request, response);
+    }
 
 
 
